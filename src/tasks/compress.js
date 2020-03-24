c5827959e029aa8d74984cf9e4529164284efb30
Bas de Greeuw <bas.de.greeuw@van-ons.nl>
2020-03-24 21:07:07 +0100

    Fixed compress error

--- src/tasks/compress.js
+++ src/tasks/compress.js
@@ -1,8 +1,6 @@
 const fs = require ( 'fs-extra' );
 const zlib = require ( 'zlib' );
 const brotli = require ( 'brotli' );
-const { stationsPath } = require ( "../generateData" );
-const { getJson } = require ( "../generateData" );
 
 const brotliSettings = {
     extension: 'br',
@@ -14,23 +12,21 @@ const brotliSettings = {
 
 const dirs = [ 'public', 'public/404', 'public/offline' ];
 
-getJson ( stationsPath ).then ( stations => {
-    dirs.forEach ( dir => {
-        fs.readdirSync ( dir ).forEach ( file => {
-            if ( file.endsWith ( '.js' ) || file.endsWith ( '.css' ) || file.endsWith ( '.html' ) ) {
-                // brotli
-                const result = brotli.compress ( fs.readFileSync ( dir + '/' + file ), brotliSettings );
-                fs.writeFileSync ( dir + '/' + file + '.br', result );
-                // gzip
-                const fileContents = fs.createReadStream ( dir + '/' + file );
-                const writeStream = fs.createWriteStream ( dir + '/' + file + '.gz' );
-                const zip = zlib.createGzip ();
-                fileContents
-                    .pipe ( zip )
-                    .on ( 'error', err => console.error ( err ) )
-                    .pipe ( writeStream )
-                    .on ( 'error', err => console.error ( err ) );
-            }
-        } )
-    } );
+dirs.forEach ( dir => {
+    fs.readdirSync ( dir ).forEach ( file => {
+        if ( file.endsWith ( '.js' ) || file.endsWith ( '.css' ) || file.endsWith ( '.html' ) ) {
+            // brotli
+            const result = brotli.compress ( fs.readFileSync ( dir + '/' + file ), brotliSettings );
+            fs.writeFileSync ( dir + '/' + file + '.br', result );
+            // gzip
+            const fileContents = fs.createReadStream ( dir + '/' + file );
+            const writeStream = fs.createWriteStream ( dir + '/' + file + '.gz' );
+            const zip = zlib.createGzip ();
+            fileContents
+                .pipe ( zip )
+                .on ( 'error', err => console.error ( err ) )
+                .pipe ( writeStream )
+                .on ( 'error', err => console.error ( err ) );
+        }
+    } )
 } );
